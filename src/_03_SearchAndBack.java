cc61dd2ccd713aee5f60fa7995b13517823b5cc9
ugurinanmaz <ugurinanmaz@gmail.com>
2022-07-23 19:51:06 -0500

    new

--- src/_03_SearchAndBack.java
+++ src/_03_SearchAndBack.java
@@ -3,6 +3,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class _03_SearchAndBack {
     public static void main(String[] args) throws InterruptedException {
 
@@ -10,7 +12,7 @@ public class _03_SearchAndBack {
 
         System.setProperty("webdriver.chrome.driver","C:\\Users\\inanmaz\\OneDrive - HP Inc\\Documents\\Selenium\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
 
         String searchText = "teddy bear";
@@ -25,25 +27,22 @@ public class _03_SearchAndBack {
         WebElement searchCountLocator = driver.findElement(By.cssSelector("#searchMessageContainer > div > span"));
         String searchCountText =  searchCountLocator.getText();
         System.out.println("searchCountText = " + searchCountText);
-        Thread.sleep(3000);
 
         String strUrl = driver.getCurrentUrl();
         System.out.println("Current Page URL = " + strUrl);
-        ///Thread.sleep(3000);
-        //WebElement clickSnapDeal = driver.findElement(By.cssSelector("img[title='Snapdeal'][class='notIeLogoHeader aspectRatioEqual sdLogo cur-pointer']"));
-        WebElement clickSnapDeal = driver.findElement(By.cssSelector(".notIeLogoHeader.aspectRatioEqual.sdLogo.cur-pointer"));
+
+        WebElement clickSnapDeal = driver.findElement(By.cssSelector("img[title='Snapdeal'][class='notIeLogoHeader aspectRatioEqual sdLogo cur-pointer']"));
+//        WebElement clickSnapDeal = driver.findElement(By.cssSelector(".notIeLogoHeader.aspectRatioEqual.sdLogo.cur-pointer"));
         clickSnapDeal.click();
 
         String strUrl2 = driver.getCurrentUrl();
         System.out.println("Home Page URL = " + strUrl2);
 
         driver.navigate().back();
-        //Thread.sleep(3000);
+
         WebElement currentValue = driver.findElement(By.id("inputValEnter"));
-        //Thread.sleep(3000);
         String currentValueText = currentValue.getAttribute("value");
         System.out.println(currentValueText);
-        //Thread.sleep(5000);
 
         if (searchText.equals(currentValueText)){
             System.out.println("Passed!");
